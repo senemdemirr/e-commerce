11e4839826943d0b663353aec77c772aaaa806a0
senemdemirr <senemdemir262@gmail.com>
2025-07-11 00:25:40 +0300

    imported and used components

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,7 +22,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <Header/>
+        <main className="container mx-auto px-4">
+          {children}
+        </main>
+        <Footer/>
       </body>
     </html>
   );
