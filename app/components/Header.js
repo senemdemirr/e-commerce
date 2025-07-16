364483c8059f02aa34e41a02f4e68249a9a4d4a0
senemdemirr <senemdemir262@gmail.com>
2025-07-16 16:46:40 +0300

    AuthMenu component imported

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,14 +2,14 @@ import Image from "next/image";
 import SearchInput from "./SearchInput";
 import Navbar from "./Navbar";
 import { Button } from "@mui/material";
-import PersonIcon from "@mui/icons-material/Person";
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import AuthMenu from "./AuthMenu";
 
 export default function Header(){
     return (
        <div className="container w-full m-auto flex flex-col ">
-         <div className=" bg-transparent flex flex-row justify-center items-center">
+         <div className=" bg-transparent flex flex-row justify-between items-center">
             <Image
             src={"/e-commerce_logo.png"}
             alt="E-Commerce Logo"
@@ -19,31 +19,18 @@ export default function Header(){
             priority
             />
             <SearchInput></SearchInput>
-            <div>
+            <div className="flex flex-row justify-center items-center ">
+                <AuthMenu></AuthMenu>
                 <Button
-                    variant="contained"
-                    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mr-2 border-none cursor-pointer"
+                    className="text-white px-4 py-2 rounded hover:bg-blue-600 mr-2 border-none cursor-pointer"
+                    startIcon={<FavoriteBorderOutlinedIcon />}
                 > 
-                <PersonIcon
-                    className="mr-2"
-                ></PersonIcon>
-                Sign In</Button>
-                <Button
-                    variant="contained"
-                    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mr-2 border-none cursor-pointer"
-                > 
-                <FavoriteBorderOutlinedIcon
-                    className="mr-2"
-                ></FavoriteBorderOutlinedIcon>
                 My Favorite
                 </Button> 
                 <Button
-                    variant="contained"
-                    className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 border-none cursor-pointer"
+                    className="text-white px-4 py-2 rounded hover:bg-green-600 border-none cursor-pointer"
+                    startIcon={<ShoppingCartOutlinedIcon />}
                 > 
-                <ShoppingCartOutlinedIcon
-                    className="mr-2"
-                ></ShoppingCartOutlinedIcon>
                 My Basket
                 </Button>
             </div>
