b7d1dc95287cea018acd9cd6bcaf573ea1af38c2
senemdemirr <senemdemir262@gmail.com>
2025-08-05 21:09:12 +0300

    added product list component

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import ProductList from "./components/ProductList";
 export default function Home() {
   return (
     <>
-     
+     <ProductList></ProductList>
     </>
   );
 }
