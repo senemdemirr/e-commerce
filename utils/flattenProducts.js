02077c4b57b255dae53907858c55f7d6c356d78b
senemdemirr <senemdemir262@gmail.com>
2025-08-18 13:37:58 +0300

    code reviewed, edited and added comment line

--- utils/flattenProducts.js
+++ utils/flattenProducts.js
@@ -1,19 +1,16 @@
 export function flattenProducts(data) {
     const product = [];
-    data.categories.forEach(category => {
-        category.children.forEach(item => {
-            item.products.forEach(productItem => {
-                product.push({
-                    ...productItem,
-                    id: productItem.id,
-                    code: productItem.sku,
-                    title: productItem.title,
-                    description: productItem.description,
-                    price: productItem.price,
-                    image: productItem.image
-                })
+    data.categories.forEach((category) => {
+        category.children.forEach((item) => {
+            item.products.forEach((productItem) => {
+                product.push( productItem) 
+                // here we are pushing the productItem into the product array(without changing productItem)
+                // if I use spread operator here, it will not work because when I use spread operator data will be an independent object
+                // the function of the spread operator is to copy the properties of an object into another object
+                // so if I use spread operator, the code should be updated like this( product.push({...productItem})
             })
         })
     })
+    console.log("flattened products", product);
     return product;
 }
