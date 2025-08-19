3e052cd411865539d9feae58c4709c1015792b3e
senemdemirr <senemdemir262@gmail.com>
2025-08-19 15:06:09 +0300

    changed comment line

--- utils/flattenProducts.js
+++ utils/flattenProducts.js
@@ -4,10 +4,7 @@ export function flattenProducts(data) {
         category.children.forEach((item) => {
             item.products.forEach((productItem) => {
                 product.push( {...productItem}) 
-                // here we are pushing the productItem into the product array(without changing productItem)
-                // if I use spread operator here, it will not work because when I use spread operator data will be an independent object
-                // the function of the spread operator is to copy the properties of an object into another object
-                // so if I use spread operator, the code should be updated like this( product.push({...productItem})
+                // The spread operator is used to create a shallow copy of the productItem object.
                 // The spread operator was used because it is more powerful.
             })
         })
