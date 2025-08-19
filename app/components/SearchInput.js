e31d00e66ef520ef356699bb2611fecc25667a09
senemdemirr <senemdemir262@gmail.com>
2025-08-19 15:04:11 +0300

    removed underline

--- app/components/SearchInput.js
+++ app/components/SearchInput.js
@@ -6,7 +6,8 @@ export default function SearchInput(){
      <Input
         type="text"
         placeholder="Search products..."
-        className="w-full max-w-md border-gray-300 border-1 rounded-sm px-2 py-1 shadow-none outline-none flex justify-between items-center"
+        disableUnderline
+        className="w-full max-w-md border-gray-300 border-1 rounded-sm px-2 py-1 shadow-none flex justify-between items-center"
         style={{ margin: "0 auto"}}
         inputProps={{ "aria-label": "Search products" }}
         endAdornment={<SearcIcon></SearcIcon>}
