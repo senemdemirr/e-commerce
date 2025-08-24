77af44197f9c3fd288c405ed70d9424b8d677732
senemdemirr <senemdemir262@gmail.com>
2025-08-24 19:09:50 +0300

    search input filter completed

--- app/components/ProductList.js
+++ app/components/ProductList.js
@@ -1,13 +1,29 @@
+"use client";
 import { flattenProducts } from "@/utils/flattenProducts.js";
 import dataFromApi from "@/lib/data/data.json";
 import ProductCard from "./ProductCard";
+import { useSearchParams } from "next/navigation";
+import { useMemo } from "react";
 
 export default function ProductList() {
-    const data = flattenProducts(dataFromApi);
+    const data = useMemo(() => flattenProducts(dataFromApi) , []);
+    // I used useMemo for unnecessary renders to obstruct and for performance optimization
+    const searchParams = useSearchParams();
+    const query = searchParams.get("query") || "";
+    const queryValue = query.trim().toLocaleLowerCase('tr');
+    // 'tr' locale is used for Turkish characters like 'i' and 'I'
+
+    const filtered = useMemo(() => {
+        if(!queryValue){
+            return data;
+        }
+        return data.filter((product) => product.title.toLocaleLowerCase('tr').includes(queryValue));
+    }, [data, queryValue]);
+    
     return(
         <div className="py-8">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                {data.map((product) => (
+                {filtered.map((product) => (
                     <ProductCard key={product.id} product={product}></ProductCard>
                 ))}
             </div>
--- app/components/SearchInput.js
+++ app/components/SearchInput.js
@@ -1,16 +1,37 @@
+"use client";
 import { Input } from "@mui/material";
-import SearcIcon from "@mui/icons-material/Search";
+import SearchIcon from "@mui/icons-material/Search";
+import { useState } from "react";
+import { useRouter , useSearchParams, usePathname} from "next/navigation";
 
-export default function SearchInput(){
-   return(
-     <Input
-        type="text"
-        placeholder="Search products..."
-        disableUnderline
-        className="w-full max-w-md border-gray-300 border-1 rounded-sm px-2 py-1 shadow-none flex justify-between items-center"
-        style={{ margin: "0 auto"}}
-        inputProps={{ "aria-label": "Search products" }}
-        endAdornment={<SearcIcon></SearcIcon>}
-    />
+export default function SearchInput() {
+
+   const [searchValue, setSearchValue] = useState("");
+   const searchParams = useSearchParams();
+   const pathName = usePathname();
+   const router = useRouter();
+
+   function handleQueryChange(query) {
+      router.push(`${pathName}?${new URLSearchParams({ ...Object.fromEntries(searchParams.entries()), query }).toString()}`);
+   }
+   function onSubmit(e) {
+      e.preventDefault();
+      handleQueryChange(searchValue.trim());
+   }
+
+
+   return (
+      <form onSubmit={onSubmit} role="search" className="w-full max-w-md border-gray-300 border-1 rounded-sm px-2 py-1 shadow-none ">
+         <Input
+            type="search"
+            placeholder="Search products..."
+            onChange={(e) => setSearchValue(e.target.value)}
+            disableUnderline
+            className="w-full"
+            style={{ margin: "0 auto" }}
+            inputProps={{ "aria-label": "Search products" }}
+            endAdornment={<SearchIcon></SearchIcon>}
+         />
+      </form>
    )
 }
