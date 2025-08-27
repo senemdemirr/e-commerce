b7861c6b14428671d62c714a921df642a19ded25
senemdemirr <senemdemir262@gmail.com>
2025-08-27 23:26:41 +0300

    edited Link

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,43 +1,52 @@
 import Image from "next/image";
+import Link from "next/link";
 import SearchInput from "./SearchInput";
 import Navbar from "./Navbar";
-import { Button , Link} from "@mui/material";
+import { Button } from "@mui/material";
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import AuthMenu from "./AuthMenu";
 
 export default function Header(){
     return (
-       <div className="container w-full mx-auto flex flex-col ">
-         <div className=" bg-transparent flex flex-row justify-between items-center">
-            <Link href="/">
+       <div className="container w-full mx-auto flex flex-col">
+         <div className="bg-transparent flex flex-row justify-between items-center">
+            <Link href="/" passHref>
                 <Image
-                src={"/e-commerce_logo.png"}
-                alt="E-Commerce Logo"
-                width={200}
-                height={50}
-                className="cursor-pointer"
-                priority
+                    src="/e-commerce_logo.png"
+                    alt="E-Commerce Logo"
+                    width={200}
+                    height={50}
+                    className="cursor-pointer"
+                    priority
                 />
             </Link>
-            <SearchInput></SearchInput>
-            <div className="flex flex-row justify-center items-center ">
-                <AuthMenu></AuthMenu>
-                <Button
-                    className="!text-gray-600 px-4 py-2 rounded hover:!bg-transparent mr-2 border-none cursor-pointer"
-                    startIcon={<FavoriteBorderOutlinedIcon />}
-                > 
-                My Favorite
-                </Button> 
-                <Button
-                    className="!text-gray-600 px-4 py-2 rounded hover:!bg-transparent border-none cursor-pointer"
-                    startIcon={<ShoppingCartOutlinedIcon />}
-                > 
-                My Basket
-                </Button>
+            <SearchInput />
+            <div className="flex flex-row justify-center items-center">
+                <AuthMenu />
+                {/* I used button inside Link for style and icon 
+                  passHref passes href information to the button
+                */}
+
+                <Link href="/favorites" passHref>
+                    <Button
+                        className="!text-gray-600 px-4 py-2 rounded hover:!bg-transparent mr-2 border-none cursor-pointer"
+                        startIcon={<FavoriteBorderOutlinedIcon />}
+                    >
+                        My Favorite
+                    </Button>
+                </Link>
+                <Link href="/basket" passHref>
+                    <Button
+                        className="!text-gray-600 px-4 py-2 rounded hover:!bg-transparent border-none cursor-pointer"
+                        startIcon={<ShoppingCartOutlinedIcon />}
+                    >
+                        My Basket
+                    </Button>
+                </Link>
             </div>
         </div> 
-        <Navbar></Navbar>
+        <Navbar />
        </div>   
     );
 }
