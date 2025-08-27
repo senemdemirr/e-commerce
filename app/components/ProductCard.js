7f16b9c1ce30d9306a8ada139a7d97e9ffd0c156
senemdemirr <senemdemir262@gmail.com>
2025-08-28 00:30:35 +0300

    stock information removed

--- app/components/ProductCard.js
+++ app/components/ProductCard.js
@@ -31,7 +31,6 @@ export default function ProductCard({ product }) {
                     <span className="text-gray-600 text-xs">{product.description}</span>
                     <div className="flex items-end justify-between mt-2">
                         <span className="text-xl font-bold text-orange-400">{product.price} <span className="text-base font-medium">{product.currency}</span></span>
-                        <span className="text-xs text-gray-400">{product.stock} adet</span>
                     </div>
                 </div>
             </div>
