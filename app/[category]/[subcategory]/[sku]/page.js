50263a19bf4476fb06fb67c6e81d7a0089f482a9
senemdemirr <senemdemir262@gmail.com>
2025-08-28 00:49:11 +0300

    added product details

--- app/[category]/[subcategory]/[sku]/page.js
+++ app/[category]/[subcategory]/[sku]/page.js
@@ -1,3 +1,42 @@
-export default function ProductDetailPage() {
-  return <div>Product Detail Page</div>;
+import data from "@/lib/data/data.json";
+import { flattenProducts } from "@/utils/flattenProducts";
+import { Button } from "@mui/material";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
+export default async function ProductDetailPage({ params }) {
+    const { category, subcategory, sku } = await params;
+    const allProducts = flattenProducts(data);
+    const product = allProducts.find(p => p.categorySlug === category && p.subCategorySlug === subcategory && p.sku === sku);
+
+    if (!product) {
+        return notFound();
+    }
+
+    return (
+        <div className="container mx-auto px-4 py-8">
+            <nav className="text-sm mb-4 text-gray-500">
+                <Link href={`/${category}`} className="hover:underline">{category}</Link>
+                <Link href={`/${subcategory}`} className="hover:underline">/{subcategory}</Link>
+                <span className="text-gray-700"> /{sku} </span>
+            </nav>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                <div className="bg-gray-50 p-4 rounded-lg flex items-center justify-center">
+                    <img src={product.image} alt={product.title} className="max-h-96 object-contain" />
+                </div>
+                <div>
+                    <h1 className="text-3xl font-bold mb-2">{product.title}</h1>
+                    <p className="text-gray-600 mb-4">{product.brand}</p>
+                    <p className="text-gray-800 mb-6">{product.description}</p>
+                    <span className="text-2xl font-semibold text-orange-500">{product.price} <span className="text-lg font-medium">{product.currency}</span></span>
+                    <div className="mt-6">
+                        <Button className="!bg-orange-500 !text-white !px-6 py-2 rounded-lg hover:bg-orange-600 transition" startIcon={<AddShoppingCartIcon></AddShoppingCartIcon>}>Add to Cart</Button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+    
 }
