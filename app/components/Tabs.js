ecfb5d08629f16b36646dbefee7bb14749ce8487
senemdemirr <senemdemir262@gmail.com>
2025-08-27 15:53:57 +0300

    added cursor

--- app/components/Tabs.js
+++ app/components/Tabs.js
@@ -19,13 +19,13 @@ export default function Tabs() {
       <div className="tabs-header flex w-full justify-between">
         <button
           onClick={() => handleTabChange(0)}
-          className={`tab w-50 px-4 py-2 ${tabIndex === 0 ? "font-bold border-b-2 border-orange-500" : ""}`}
+          className={`tab w-50 px-4 py-2 cursor-pointer ${tabIndex === 0 ? "font-bold border-b-2 border-orange-500" : ""}`}
         >
           Sign In
         </button>
         <button
           onClick={() => handleTabChange(1)}
-          className={`tab w-50 px-4 py-2 ${tabIndex === 1 ? "font-bold border-b-2 border-orange-500" : ""}`}
+          className={`tab w-50 px-4 py-2 cursor-pointer ${tabIndex === 1 ? "font-bold border-b-2 border-orange-500" : ""}`}
         >
           Sign Up
         </button>
