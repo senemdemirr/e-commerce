df799e1fc3487ee4511dbbc75570b0afd51da2fc
senemdemirr <senemdemir262@gmail.com>
2025-08-28 00:24:44 +0300

    added href for each product

--- app/components/ProductCard.js
+++ app/components/ProductCard.js
@@ -1,36 +1,40 @@
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import { useState } from 'react';
+import Link from 'next/link';
 export default function ProductCard({ product }) {
     const [isFavorite, setIsFavorite] = useState(false);
+    const href = `/${product.categorySlug}/${product.subCategorySlug}/${product.sku}`;
 
     function toggleFavorite() {
         setIsFavorite(!isFavorite);
     }
     return (
-        <div className="bg-white cursor-pointer rounded-2xl shadow-md hover:shadow-xl transition flex flex-col overflow-hidden border border-gray-100">
-            <div className="relative">
-                <img
-                    src={product.image}
-                    alt={product.title}
-                    className="h-64 w-full object-contain bg-white p-2"
-                />
-                <span onClick={toggleFavorite} className="absolute top-2 right-2 bg-gray-100 border border-gray-200 shadow-2xl text-white text-xs px-1.5 py-1.5 rounded-full">
-                    {isFavorite ? 
-                        <FavoriteIcon fontSize="medium" sx={{color:'orange'}} /> :
-                        <FavoriteBorderIcon fontSize="medium" color='action' />
-                    }
-                </span>
-            </div>
-            <div className="flex-1 flex flex-col px-4 py-3 gap-1">
-                <h3 className="font-semibold text-md truncate">{product.title}</h3>
-                <span className="text-sm text-gray-500">{product.brand}</span>
-                <span className="text-gray-600 text-xs">{product.description}</span>
-                <div className="flex items-end justify-between mt-2">
-                    <span className="text-xl font-bold text-orange-400">{product.price} <span className="text-base font-medium">{product.currency}</span></span>
-                    <span className="text-xs text-gray-400">{product.stock} adet</span>
+        <Link href={href}>
+            <div className="bg-white cursor-pointer rounded-2xl shadow-md hover:shadow-xl transition flex flex-col overflow-hidden border border-gray-100">
+                <div className="relative">
+                    <img
+                        src={product.image}
+                        alt={product.title}
+                        className="h-64 w-full object-contain bg-white p-2"
+                    />
+                    <span onClick={toggleFavorite} className="absolute top-2 right-2 bg-gray-100 border border-gray-200 shadow-2xl text-white text-xs px-1.5 py-1.5 rounded-full">
+                        {isFavorite ?
+                            <FavoriteIcon fontSize="medium" sx={{ color: 'orange' }} /> :
+                            <FavoriteBorderIcon fontSize="medium" color='action' />
+                        }
+                    </span>
+                </div>
+                <div className="flex-1 flex flex-col px-4 py-3 gap-1">
+                    <h3 className="font-semibold text-md truncate">{product.title}</h3>
+                    <span className="text-sm text-gray-500">{product.brand}</span>
+                    <span className="text-gray-600 text-xs">{product.description}</span>
+                    <div className="flex items-end justify-between mt-2">
+                        <span className="text-xl font-bold text-orange-400">{product.price} <span className="text-base font-medium">{product.currency}</span></span>
+                        <span className="text-xs text-gray-400">{product.stock} adet</span>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 }
