fa58e4284a8ca53b479d0551c0103e90dbf952d4
senemdemirr <senemdemir262@gmail.com>
2025-08-27 21:43:00 +0300

    removed async await

--- app/[category]/[subcategory]/page.js
+++ app/[category]/[subcategory]/page.js
@@ -2,8 +2,8 @@ import dataFromApi from "@/lib/data/data.json";
 import { flattenProducts } from "@/utils/flattenProducts";
 import ProductList from "../../components/ProductList";
 
-export default function SubCategoryPage({params}) {
-  const {category, subcategory} = params;
+export default async function SubCategoryPage({params}) {
+  const {category, subcategory} = await params;
   // this value names are must same dynamic page name. if not pages isn't work because isn't finded params and route
   const products = flattenProducts(dataFromApi);
   return <ProductList products={products} fixedCategorySlug={category} fixedSubCategorySlug={subcategory}></ProductList>
--- app/[category]/page.js
+++ app/[category]/page.js
@@ -2,8 +2,8 @@ import dataFromApi from "@/lib/data/data.json";
 import { flattenProducts } from "@/utils/flattenProducts";
 import ProductList from "../components/ProductList";
 
-export default function CategoryPage({params}) {
-  const {category} = params;
+export default async function CategoryPage({params}) {
+  const {category} = await params;
   const products = flattenProducts(dataFromApi);
   return <ProductList products={products} fixedCategorySlug={category}></ProductList>
 }
