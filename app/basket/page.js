69ac4bd60ab20cca9ccea76685b89f9771844090
senemdemirr <senemdemir262@gmail.com>
2025-08-27 23:26:59 +0300

    added basic page

---
+++ app/basket/page.js
@@ -0,0 +1,8 @@
+export default function BasketPage(){
+    return (
+        <div className="container mx-auto p-4">
+            <h1 className="text-2xl font-bold mb-4">My Basket</h1>
+            <p>This is where your basket items will be displayed.</p>
+        </div>
+    );
+}
---
+++ app/favorites/page.js
@@ -0,0 +1,8 @@
+export default function FavoritesPage(){
+    return (
+        <div className="container mx-auto p-4">
+            <h1 className="text-2xl font-bold mb-4">My Favorite Products</h1>
+            <p>This is where your favorite products will be displayed.</p>
+        </div>
+    );
+}
