f496132e6bb9b09a07b82ae7e3c4fb6b4bc79b93
senemdemirr <senemdemir262@gmail.com>
2025-08-29 16:42:20 +0300

    added snackbar and data saved localStroage

--- app/components/SignInForm.js
+++ app/components/SignInForm.js
@@ -1,61 +1,64 @@
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
-import { useState } from "react";
-import { TextField, Button , Snackbar} from "@mui/material";
+import { TextField, Button } from "@mui/material";
+import { useSnackbar } from "notistack";
 
 export default function SignInForm() {
-  const {register, handleSubmit, formState: {errors}} = useForm();
-  const [open, setOpen] = useState(false);
-
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const { enqueueSnackbar } = useSnackbar();
+  // this state for snackbar
   const router = useRouter();
 
   function forgotPassword() {
     router.push("/forgot-password");
   }
   const onSubmit = (data) => {
-    setOpen(true);
-    setTimeout(() => {
-    router.push("/");
-    }, 2000);
+    const getData = JSON.parse(localStorage.getItem("user"));
+    if (getData?.email !== data.email || getData?.password !== data.password) {
+      enqueueSnackbar("Invalid email or password", { variant: "error" });
+      return;
+    }
+    else {
+      enqueueSnackbar("Sign in successful", { variant: "success" });
+      setTimeout(() => {
+        router.push("/");
+      }, 2000);
+    }
   };
 
   return (
-   <>
-    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 px-4">
-      <TextField 
-        {...register("email",  
-          {
-            required: "Email is required" , 
-            pattern:{
-              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-              message: "Invalid email address" 
-            }
-        })}
-        error={!!errors.email}
-        helperText={errors.email ? errors.email.message : ""}
-        size="small" 
-        label="Email" 
-        fullWidth 
-        className="!my-2"
-      />
-      <TextField 
-        {...register("password", { required: "Password is required" })}
-        error={!!errors.password}
-        helperText={errors.password ? errors.password.message : ""}
-        size="small" 
-        label="Password" 
-        type="password" 
-        fullWidth 
-        className="!my-2"
-      />
-      <span onClick={forgotPassword} className="hover:underline cursor-pointer mt-2 float-right text-gray-600">I forgot my password</span>
-      <Button type="submit" variant="contained" className="w-full !bg-green-500 !my-4">Sign In</Button>
-    </form>
-    <Snackbar
-      open={open}
-      autoHideDuration={1000}
-      message="Sign in successful! Redirecting..."
-    />
+    <>
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 px-4">
+        <TextField
+          {...register("email",
+            {
+              required: "Email is required",
+              pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                message: "Invalid email address"
+              }
+            })}
+          error={!!errors.email}
+          helperText={errors.email ? errors.email.message : ""}
+          size="small"
+          label="Email"
+          fullWidth
+          className="!my-2"
+        />
+        <TextField
+          {...register("password", { required: "Password is required" })}
+          error={!!errors.password}
+          helperText={errors.password ? errors.password.message : ""}
+          size="small"
+          label="Password"
+          type="password"
+          fullWidth
+          className="!my-2"
+        />
+        <span onClick={forgotPassword} className="hover:underline cursor-pointer mt-2 float-right text-gray-600">I forgot my password</span>
+        <Button type="submit" variant="contained" className="w-full !bg-green-500 !my-4">Sign In</Button>
+      </form>
+      
     </>
   );
 }
--- app/components/SignUpForm.js
+++ app/components/SignUpForm.js
@@ -1,15 +1,21 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
-import { TextField, Button , Snackbar} from "@mui/material";
+import { TextField, Button, Snackbar } from "@mui/material";
 
 export default function SignUpForm() {
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
   const password = watch("password");
+  //this value for confirm password validation
   const [open, setOpen] = useState(false);
+  // this state for snackbar
   const router = useRouter();
 
   const onSubmit = (data) => {
+    const user = { email: data.email, password: data.password };
+    localStorage.setItem("user", JSON.stringify(user));
+    
     setOpen(true);
     setTimeout(() => {
       router.push("/");
@@ -18,65 +24,65 @@ export default function SignUpForm() {
 
   return (
     <>
-    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 px-4">
-      <TextField 
-        size="small"
-        className="!my-2"
-        label="Email" 
-        fullWidth 
-        {...register("email",  
-          {
-            required: "Email is required" , 
-            pattern:{
-              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-              message: "Invalid email address" 
-            }
-        })}
-        error={!!errors.email}
-        helperText={errors.email ? errors.email.message : ""}
-      />
-      <TextField 
-        size="small"
-        className="!my-2"
-        label="Password" 
-        type="password" 
-        fullWidth 
-        {...register("password", 
-          { 
-            required: "Password is required",
-            minLength: {
-              value: 6,
-              message: "Password must be at least 6 characters"
-            },
-            maxLength: {
-              value: 11,
-              message: "Password must not exceed 10 characters"
-            }
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 px-4">
+        <TextField
+          size="small"
+          className="!my-2"
+          label="Email"
+          fullWidth
+          {...register("email",
+            {
+              required: "Email is required",
+              pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                message: "Invalid email address"
+              }
+            })}
+          error={!!errors.email}
+          helperText={errors.email ? errors.email.message : ""}
+        />
+        <TextField
+          size="small"
+          className="!my-2"
+          label="Password"
+          type="password"
+          fullWidth
+          {...register("password",
+            {
+              required: "Password is required",
+              minLength: {
+                value: 6,
+                message: "Password must be at least 6 characters"
+              },
+              maxLength: {
+                value: 11,
+                message: "Password must not exceed 10 characters"
+              }
+            })}
+          error={!!errors.password}
+          helperText={errors.password ? errors.password.message : ""}
+        />
+        <TextField
+          size="small"
+          className="!my-2"
+          label="Confirm Password"
+          type="password"
+          fullWidth
+          {...register("confirmPassword", {
+            required: "Confirm Password is required",
+            validate: (value) => value === password || "Passwords do not match"
           })}
-        error={!!errors.password}
-        helperText={errors.password ? errors.password.message : ""}
+          error={!!errors.confirmPassword}
+          helperText={errors.confirmPassword ? errors.confirmPassword.message : ""}
+        />
+        <Button type="submit" variant="contained" className="w-full !bg-green-500 !text-gray-100 !my-4">Sign Up</Button>
+      </form>
+      <Snackbar
+        open={open}
+        autoHideDuration={2000}
+        onClose={() => setOpen(false)}
+        message="Sign up successful! Redirecting..."
       />
-      <TextField 
-        size="small"
-        className="!my-2"
-        label="Confirm Password" 
-        type="password" 
-        fullWidth 
-        {...register("confirmPassword", { 
-          required: "Confirm Password is required",
-          validate: (value) => value === password|| "Passwords do not match"
-        })}
-        error={!!errors.confirmPassword}
-        helperText={errors.confirmPassword ? errors.confirmPassword.message : ""}
-      />
-      <Button type="submit" variant="contained" className="w-full !bg-green-500 !text-gray-100 !my-4">Sign Up</Button>
-    </form>
-    <Snackbar
-      open={open}
-      autoHideDuration={2000}
-      onClose={() => setOpen(false)}
-      message="Sign up successful! Redirecting..."
-    />
     </>
   );
 }
