9e1ffb291d6f23fd3c8a93f13c4dd5fa575c966e
senemdemirr <senemdemir262@gmail.com>
2025-11-26 16:21:15 +0300

    completed for auth0

---
+++ middleware.js
@@ -0,0 +1,13 @@
+import { auth0 } from "@/lib/auth0";
+
+export async function middleware(request) {
+
+    return await auth0.middleware(request);
+}
+
+export const config = {
+    matcher: [
+        "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+    ]
+}
+
