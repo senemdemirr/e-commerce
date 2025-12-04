5b87d71d9829f9643d5a90a64df7b7a7f4e46e4b
senemdemirr <senemdemir262@gmail.com>
2025-12-04 15:08:28 +0300

    completed product detail route

--- app/api/products/[sku]/route.js
+++ app/api/products/[sku]/route.js
@@ -1,4 +1,20 @@
+import { pool } from "@/db";
 
-export async function GET(){
+export async function GET(request, { params }) {
+    //I'll use request parameter for the token
+    const { sku } = await params;
+    
+    try {
+        const res = await pool.query("SELECT p.*, sc.slug AS subcategorySlug, c.slug AS categorySlug FROM products p LEFT JOIN sub_categories sc ON p.sub_category_id = sc.id LEFT JOIN categories c ON c.id = sc.category_id WHERE p.sku = $1", [sku]);
+        const data = res.rows[0];
 
+        return Response.json(data, { status: 200 });
+
+    } catch (error) {
+        console.log("GET api/products/sku error: ", error);
+        return Response.json(
+            { message: "Something went wrong" },
+            { status: 500 }
+        )
+    }
 }
