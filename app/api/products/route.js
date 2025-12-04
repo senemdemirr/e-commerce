2b003af4f20d7ac31ffebe8ccc7451b320093958
senemdemirr <senemdemir262@gmail.com>
2025-12-04 21:36:30 +0300

    completed products route

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,15 +1,32 @@
 import { pool } from "@/db";
 
-export async function GET() {
+export async function GET(request) {
+    const { searchParams } = new URL(request.url);
+
+    const category = searchParams.get('category');
+    const subCategory = searchParams.get('subCategory');
+    let query = `SELECT p.*, sc.slug AS subcategorySlug, c.slug AS categorySlug FROM products p LEFT JOIN sub_categories sc ON p.sub_category_id = sc.id LEFT JOIN categories c ON c.id = sc.category_id WHERE 1=1`
+
+    const values = []
     try {
-        const res = await pool.query("SELECT * FROM products");
-        return Response.json(res.rows, {status: 200});
+        if(category && category.trim() !== ""){
+            values.push(category)
+            query += ` AND c.slug = $1`;
+        }
+        if(subCategory && subCategory.trim() !== 1){
+            values.push(subCategory);
+            query += ` AND sc.slug = $2`;
+        }
         
+
+        const res = await pool.query(query,values);
+        return Response.json(res.rows, { status: 200 });
+
     } catch (error) {
-        console.log("GET api/products error: ",error);
+        console.log("GET api/products error: ", error);
         return Response.json(
-            {message: "Something went wrong"},
-            {status: 500}
+            { message: "Something went wrong" },
+            { status: 500 }
         )
     }
 }
