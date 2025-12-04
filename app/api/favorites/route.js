84ff8b51529b9e53b59c0fd3a4c3537d90b6950b
senemdemirr <senemdemir262@gmail.com>
2025-12-04 23:08:01 +0300

    changed sql

--- app/api/favorites/route.js
+++ app/api/favorites/route.js
@@ -6,7 +6,7 @@ export async function GET(request) {
     const userId = searchParams.get("user");
 
     try {
-        const res = await pool.query("SELECT * FROM favorites WHERE user_id= $1", [userId]);
+        const res = await pool.query("SELECT p.title AS title, p.description AS description, p.image AS image, p.sku AS sku, p.brand AS brand, sc.slug AS subCategorySlug, c.slug AS categorySlug FROM favorites f LEFT JOIN products p ON p.id = f.product_id LEFT JOIN sub_categories sc ON sc.id = p.sub_category_id LEFT JOIN categories c ON c.id = sc.category_id WHERE user_id= $1", [userId]);
         return Response.json(
             res.rows,
             { status: 200 }
