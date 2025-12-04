3a005ec0a4580b8e966633b6941677db771e065f
senemdemirr <senemdemir262@gmail.com>
2025-12-04 22:45:00 +0300

    completed favorites route

--- app/api/favorites/route.js
+++ app/api/favorites/route.js
@@ -1,18 +1,22 @@
 import { pool } from "@/db";
 
-export async function GET() {
+export async function GET(request) {
+
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("user");
+
     try {
-        const res = await pool.query("SELECT * FROM favorites WHERE user_id= $1", [user]);
+        const res = await pool.query("SELECT * FROM favorites WHERE user_id= $1", [userId]);
         return Response.json(
             res.rows,
-            {status: 200}
+            { status: 200 }
         )
-        
+
     } catch (error) {
         console.log("GET /api/favorites error: ", error);
         return Response.json(
-            {message: "Something went wrong."},
-            {status: 500}
+            { message: "Something went wrong." },
+            { status: 500 }
         )
     }
 }
