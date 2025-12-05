044356f2ab3aacf18cc941aa66eafe370ec3fd7a
senemdemirr <senemdemir262@gmail.com>
2025-12-05 21:03:36 +0300

    bug fix

--- app/api/products/[sku]/route.js
+++ app/api/products/[sku]/route.js
@@ -3,10 +3,19 @@ import { pool } from "@/db";
 export async function GET(request, { params }) {
     //I'll use request parameter for the token
     const { sku } = await params;
-    
+    if (!sku || sku.endsWith(".json")) {
+        // Chrome devtools vs. buraya düştüğünde 204 dön, hiç hata verme
+        return new Response(null, { status: 204 });
+    }
+
     try {
         const res = await pool.query(`SELECT p.title,p.brand,p.price,p.image,p.sku, sc.slug AS "subCategorySlug", c.slug AS "categorySlug" FROM products p LEFT JOIN sub_categories sc ON p.sub_category_id = sc.id LEFT JOIN categories c ON c.id = sc.category_id WHERE p.sku = $1`, [sku]);
-
+        if (res.rows.length === 0) {
+            return new Response(
+                JSON.stringify({ message: "Product not found" }),
+                { status: 404, headers: { "Content-Type": "application/json" } }
+            );
+        }
         const data = await res.rows[0];
 
         return Response.json(data, { status: 200 });
