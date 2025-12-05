d7851061fdc0a4082d3c059169d92ac7473c743d
senemdemirr <senemdemir262@gmail.com>
2025-12-05 19:52:21 +0300

    bug fix

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -4,7 +4,7 @@ export async function GET(request) {
     const { searchParams } = new URL(request.url);
 
     const category = searchParams.get('category');
-    const subCategory = searchParams.get('subCategory');
+    const subCategory = searchParams.get('subcategory');
     let query = `SELECT p.*, sc.slug AS subCategorySlug, c.slug AS categorySlug FROM products p LEFT JOIN sub_categories sc ON p.sub_category_id = sc.id LEFT JOIN categories c ON c.id = sc.category_id WHERE 1=1`
 
     const values = []
