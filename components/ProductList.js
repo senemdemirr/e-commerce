3c5a970ca8cdcb76e37c85d26ad18fb3cf23748a
senemdemirr <senemdemir262@gmail.com>
2025-12-06 19:25:16 +0300

    added if condition

--- components/ProductList.js
+++ components/ProductList.js
@@ -18,15 +18,14 @@ export default function ProductList({products}) {
         return list;
     }, [products,queryValue]);
     //Rerun the filter only if the values ​​in this list change, otherwise use the old result.
-
-    if(filtered.length === 0){
+    if(filtered?.length === 0){
         return <div className="py-8 text-center text-gray-500">No products found</div>
     }
     
     return(
         <div className="py-8">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                {filtered.map((product) => (
+                {filtered?.map((product) => (
                     <ProductCard key={product.id} product={product}></ProductCard>
                 ))}
             </div>
