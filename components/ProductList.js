1408f120b8f3b5bed33173a0185fa985e1c8b939
senemdemirr <senemdemir262@gmail.com>
2025-12-07 01:36:28 +0300

    format document and added router push when not found favorite

--- components/ProductList.js
+++ components/ProductList.js
@@ -1,9 +1,10 @@
 "use client";
 import ProductCard from "./ProductCard";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 import { useMemo } from "react";
 
-export default function ProductList({products}) {
+export default function ProductList({ products }) {
+    const router = useRouter();
     const searchParams = useSearchParams();
     const query = searchParams.get("query") || "";
     const queryValue = query.trim().toLocaleLowerCase('tr');
@@ -12,17 +13,19 @@ export default function ProductList({products}) {
     // I used useMemo for unnecessary renders to obstruct and for performance optimization
     const filtered = useMemo(() => {
         let list = products;
-        if(queryValue){
+        if (queryValue) {
             list = list.filter((product) => product.title.toLocaleLowerCase('tr').includes(queryValue))
         }
         return list;
-    }, [products,queryValue]);
+    }, [products, queryValue]);
     //Rerun the filter only if the values ​​in this list change, otherwise use the old result.
-    if(filtered?.length === 0){
-        return <div className="py-8 text-center text-gray-500">No products found</div>
+    if (filtered?.length === 0) {
+        // return <div className="py-8 text-center text-gray-500">No products found</div>
+        router.push("/auth/login");
+
     }
-    
-    return(
+
+    return (
         <div className="py-8">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                 {filtered?.map((product) => (
