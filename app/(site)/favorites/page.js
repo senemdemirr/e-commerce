708913622748ae4b6717075f6b26b10abe287725
senemdemirr <senemdemir262@gmail.com>
2025-12-06 19:24:38 +0300

    completed get all favorites

--- app/(site)/favorites/page.js
+++ app/(site)/favorites/page.js
@@ -1,9 +1,15 @@
-import { flattenProducts } from "@/utils/flattenProducts";
-import dataFromApi from "@/lib/data/data.json";
 import ProductList from "@/components/ProductList";
+import { apiFetch } from "@/lib/apiFetch/fetch";
 import { Suspense } from "react";
-export default function FavoritesPage() {
-    const products = flattenProducts(dataFromApi);
+
+async function fetchFavorite(userId) {
+    const searchParams = new URLSearchParams();
+    if (userId) searchParams.set("userId", userId);
+
+    return await apiFetch(`/api/favorites?${searchParams.toString()}`);
+}
+export default async function FavoritesPage() {
+    const products = await fetchFavorite(1);
     return (
         <Suspense fallback={null}>
             <ProductList products={products} isFavorite={true}></ProductList>
