a8bfc248cd3b4a55839e4f62589eece0990e2f41
senemdemirr <senemdemir262@gmail.com>
2025-12-07 17:02:37 +0300

    added new ıcon for favorite page

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,5 +1,6 @@
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import CloseIcon from '@mui/icons-material/Close';
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -15,9 +16,12 @@ export default function ProductCard({ product }) {
     }, [pathname]);
 
 
-    function toggleFavorite(e) {
+    function updateFavorite(e) {
         e.preventDefault();
         setIsFavorite(!isFavorite);
+    }
+    function deleteFavorite(e) {
+        e.preventDefault();
     }
     return (
         <Link prefetch={false}
@@ -32,11 +36,20 @@ export default function ProductCard({ product }) {
                         width={300}
                         height={300}
                     />
-                    <span onClick={toggleFavorite} className="absolute top-2 right-2 bg-gray-100 border border-gray-200 shadow-2xl text-white text-xs px-1.5 py-1.5 rounded-full">
-                        {isFavorite ?
-                            <FavoriteIcon fontSize="medium" sx={{ color: 'orange' }} /> :
-                            <FavoriteBorderIcon fontSize="medium" color='action' />
+                    <span className="absolute top-2 right-2 bg-gray-100 border border-gray-200 shadow-2xl text-white text-xs px-1.5 py-1.5 rounded-full">
+                        {pathname == "/favorites" ?
+                            (<CloseIcon onClick={deleteFavorite} fontSize="medium" color='action'></CloseIcon>) :
+                            (
+                                isFavorite ?
+                                    (<FavoriteIcon onClick={updateFavorite} fontSize="medium" sx={{ color: 'orange' }} />)
+                                    :
+                                    (<FavoriteBorderIcon onClick={updateFavorite} fontSize="medium" color='action' />)
+
+                            )
                         }
+
+
+
                     </span>
                 </div>
                 <div className="flex-1 flex flex-col px-4 py-3 gap-1">
