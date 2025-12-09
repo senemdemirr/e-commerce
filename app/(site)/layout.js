05e2dfb260035ba2badfe3b6646655014e3c5a63
senemdemirr <senemdemir262@gmail.com>
2025-12-09 22:27:15 +0300

    added control for email verify

--- app/(site)/layout.js
+++ app/(site)/layout.js
@@ -3,11 +3,13 @@ import Header from "@/components/Header";
 import { Auth0Provider } from "@auth0/nextjs-auth0";
 import { Suspense } from "react";
 import { getOrCreateUserFromSession } from "@/lib/users";
+import EmailVerified from "@/components/EmailVerified";
+import Providers from "../providers";
 
 export default async function SiteLayout({ children }) {
     const res = await getOrCreateUserFromSession();
     let user;
-    if(res.id){
+    if (res.id) {
         user = res;
     }
     return (
@@ -15,13 +17,20 @@ export default async function SiteLayout({ children }) {
             <Suspense fallback={null}>
                 <Header user={user} />
             </Suspense>
-
-            <main className="container mx-auto px-4">
-                <Auth0Provider>
-                    {children}
-                </Auth0Provider>
-            </main>
-
+            <Providers>
+                {
+                    user && (user.email_verified == false) 
+                    ?
+                    <EmailVerified></EmailVerified>
+                    :
+                    ("")
+                }
+                <main className="container mx-auto">
+                    <Auth0Provider>
+                        {children}
+                    </Auth0Provider>
+                </main>
+            </Providers>
             <Footer />
         </>
     )
