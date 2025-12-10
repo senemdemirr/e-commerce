567fe8ba0d1807bb514576cf039e7b92d92fbf14
senemdemirr <senemdemir262@gmail.com>
2025-12-10 11:44:34 +0300

    product id and favorite id were seperated

--- app/api/favorites/route.js
+++ app/api/favorites/route.js
@@ -5,7 +5,7 @@ export async function GET(request) {
     const userId = searchParams.get("userId");
 
     try {
-        const res = await pool.query(`SELECT p.id,p.title AS title, p.description AS description, p.image AS image, p.sku AS sku, p.brand AS brand,p.price AS price, sc.slug AS "subCategorySlug", c.slug AS "categorySlug" FROM favorites f LEFT JOIN products p ON p.id = f.product_id LEFT JOIN sub_categories sc ON sc.id = p.sub_category_id LEFT JOIN categories c ON c.id = sc.category_id WHERE user_id= $1`, [userId]);
+        const res = await pool.query(`SELECT p.id AS product_id, f.id AS id,p.title AS title, p.description AS description, p.image AS image, p.sku AS sku, p.brand AS brand,p.price AS price, sc.slug AS "subCategorySlug", c.slug AS "categorySlug" FROM favorites f LEFT JOIN products p ON p.id = f.product_id LEFT JOIN sub_categories sc ON sc.id = p.sub_category_id LEFT JOIN categories c ON c.id = sc.category_id WHERE user_id= $1`, [userId]);
         return Response.json(
             res?.rows,
             { status: 200 }
