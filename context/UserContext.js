a318af19b5ef3661e09844de5d018fc6e5c31c02
senemdemirr <senemdemir262@gmail.com>
2025-12-15 17:35:47 +0300

    added user context

---
+++ context/UserContext.js
@@ -0,0 +1,16 @@
+"use client";
+import { createContext, useContext } from "react"
+
+const UserContext = createContext(null);
+
+export function UserProvider({ user, children }) {
+    return (
+        <UserContext.Provider value={user}>
+            {children}
+        </UserContext.Provider>
+    )
+}
+
+export function useUser(){
+    return useContext(UserContext);
+}
