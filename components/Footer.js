1f30493b33fc8f080079d8415dbd927cea4f4475
senemdemirr <senemdemir262@gmail.com>
2025-12-24 21:33:47 +0300

    changed fixed to relative

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="fixed bottom-0 w-full bg-white border-t border-gray-200 text-center py-4">
+    <footer className="relative bottom-0 w-full bg-white border-t border-gray-200 text-center py-4">
       <p className="text-sm text-gray-600">
         &copy; {new Date().getFullYear()} Iron e-commerce. All rights reserved.
       </p>
