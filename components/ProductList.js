f042332395cace414be95d5ed12d521301b35754
senemdemirr <senemdemir262@gmail.com>
2025-12-24 17:53:15 +0300

    removed padding bottom

--- components/ProductList.js
+++ components/ProductList.js
@@ -29,7 +29,7 @@ export default function ProductList({ products, isFavoritePage = false}) {
     }
 
     return (
-        <div className="container py-8 mx-auto pb-16">
+        <div className="container py-8 mx-auto">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                 {filtered?.map((product) => (
                     <ProductCard key={product.id} product={product} onDeleteFavorite={isFavoritePage ? handleDeleteFavorite : undefined}></ProductCard>
