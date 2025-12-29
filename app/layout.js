558d581ce8b5374e52245d7ded22fdd32181e9d1
senemdemirr <senemdemir262@gmail.com>
2025-12-29 15:23:47 +0300

    fix: font family

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,16 @@
 import "./globals.css";
 import Providers from "./providers";
+import { Work_Sans } from "next/font/google";
+
+const workSans = Work_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={workSans.className}>
         <Providers>{children}</Providers>
       </body>
     </html>
