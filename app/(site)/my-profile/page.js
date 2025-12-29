1aaf99df4b7815f098c3d70f440948f35fb90d9a
senemdemirr <senemdemir262@gmail.com>
2025-12-29 15:07:39 +0300

    removed unused elements

--- app/(site)/my-profile/page.js
+++ app/(site)/my-profile/page.js
@@ -1,18 +1,11 @@
 "use client";
-import { useUser } from "@auth0/nextjs-auth0";
 import { Typography } from "@mui/material";
 
 export default function MyProfile() {
-    const { user } = useUser();
 
     return (
-        <>
-            {user ? (
-                <Typography> Hello {user.name} </Typography>
-            ) : (
-                <Typography> User not found! Please first log in</Typography>
-
-            )}
-        </>
+        <div>
+            <Typography>test</Typography>
+        </div>
     )
 }
