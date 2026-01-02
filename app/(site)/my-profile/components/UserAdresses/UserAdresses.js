08a2b95321d2a0279d7efb2d5a621794ca7a2a26
senemdemirr <senemdemir262@gmail.com>
2026-01-02 23:43:17 +0300

    create user adres header

--- app/(site)/my-profile/components/UserAdresses/UserAdresses.js
+++ app/(site)/my-profile/components/UserAdresses/UserAdresses.js
@@ -1,5 +1,17 @@
-export default function UserAdresses(){
+import { Typography, Button } from "@mui/material";
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+
+export default function UserAdresses() {
     return (
-        <div>User Adressses Page</div>
+        <div className="container">
+            <div className="flex flex-row justify-between items-center">
+                <Typography className="!text-[#131614] !font-bold">Kayıtlı Adreslerim</Typography>
+                <Button
+                    variant="text"
+                    startIcon={<AddCircleOutlineIcon />}
+                    className="!rounded-lg !text-[#8DC8A1] !font-bold"
+                >Yeni Adres Ekle</Button>
+            </div>
+        </div>
     )
 }
