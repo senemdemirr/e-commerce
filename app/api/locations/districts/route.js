305fd8ed1cc6830dcd08ec0b2c687fba1f9896a0
senemdemirr <senemdemir262@gmail.com>
2026-01-05 21:07:33 +0300

    completed districts route

---
+++ app/api/locations/districts/route.js
@@ -0,0 +1,20 @@
+import { pool } from "@/db";
+
+export async function GET(request) {
+    const searchParams = new URL(request.url);
+    const cityId = searchParams.get("cityId");
+
+    try {
+        const result = await pool.query("SELECT id,name FROM districts WHERE city_id=$1", [cityId]);
+
+        return Response.json(
+            result.rows,
+            { status: 200 }
+        )
+    } catch (error) {
+        return Response.json(
+            { message: `Something went wrong: ${error}` },
+            { status: 500 }
+        )
+    }
+}
