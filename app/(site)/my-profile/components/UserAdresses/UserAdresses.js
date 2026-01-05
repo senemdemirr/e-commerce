62582e7756bafba220c23b4b63e5fef181141e36
senemdemirr <senemdemir262@gmail.com>
2026-01-05 22:23:44 +0300

    added modal

--- app/(site)/my-profile/components/UserAdresses/UserAdresses.js
+++ app/(site)/my-profile/components/UserAdresses/UserAdresses.js
@@ -1,15 +1,25 @@
 "use client";
-import { Typography, Button, CircularProgress } from "@mui/material";
+import { Typography, Button, CircularProgress, Dialog, DialogTitle, IconButton, DialogContent } from "@mui/material";
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import { useEffect, useState } from "react";
 import { apiFetch } from "@/lib/apiFetch/fetch";
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import CloseIcon from '@mui/icons-material/Close';
+import NewAdresForm from "./NewAdresForm";
 
 export default function UserAdresses() {
     const [addresses, setAddresses] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [openNewAddress, setOpenNewAddress] = useState(false);
+
+    const handleOpen = () => {
+        setOpenNewAddress(true);
+    }
+    const handleClose = () => {
+        setOpenNewAddress(false);
+    }
 
     useEffect(() => {
         const fetchAddresses = async () => {
@@ -27,13 +37,32 @@ export default function UserAdresses() {
     return (
         <div className="container">
             <div className="flex flex-row justify-between items-center mb-5">
-                <Typography className="!text-[#131614] !font-bold">Kayıtlı Adreslerim</Typography>
+                <Typography className="!text-[#131614] !font-bold">My Addresses</Typography>
                 <Button
                     variant="text"
                     startIcon={<AddCircleOutlineIcon />}
                     className="!rounded-lg !text-[#8DC8A1] !font-bold"
-                >Yeni Adres Ekle</Button>
+                    onClick={handleOpen}
+                >Add new address</Button>
             </div>
+            <Dialog
+                open={openNewAddress}
+                onClose={handleClose}
+                fullWidth
+                maxWidth="md"
+            >
+                <DialogTitle>
+                    Add new adress
+                    <IconButton onClick={handleClose}>
+                        <CloseIcon></CloseIcon>
+                    </IconButton>
+                </DialogTitle>
+                <DialogContent dividers>
+                    <NewAdresForm
+                        
+                    ></NewAdresForm>
+                </DialogContent>
+            </Dialog>
             {loading ?
                 <div className="flex flex-row justify-center items-center mt-6">
                     <CircularProgress sx={{ color: "#8DC8A1" }}></CircularProgress>
@@ -46,16 +75,16 @@ export default function UserAdresses() {
                                 <div className="group flex flex-row justify-between items-center">
                                     <div className="flex flex-row items-center">
                                         <div className="w-12 h-12 text-[#8DC8A1] bg-[#8DC8A11A] rounded-full flex items-center justify-center">
-                                            <HomeOutlinedIcon/>
+                                            <HomeOutlinedIcon />
                                         </div>
                                         <Typography className="!ml-2  !font-bold !text-base">{item.address_title}</Typography>
                                     </div>
                                     <div className="flex flex-row opacity-0 group-hover:opacity-100 transition-opacitiy duration-200">
                                         <div className="cursor-pointer text-[#6D7E73] w-10 h-10 hover:bg-[#8DC8A11A] hover:text-[#8DC8A1] rounded-full flex items-center justify-center">
-                                            <EditOutlinedIcon/>
+                                            <EditOutlinedIcon />
                                         </div>
                                         <div className="cursor-pointer text-[#6D7E73] w-10 h-10 hover:bg-red-50 hover:text-red-500 rounded-full flex items-center justify-center">
-                                            <DeleteOutlineOutlinedIcon/>
+                                            <DeleteOutlineOutlinedIcon />
                                         </div>
                                     </div>
                                 </div>
