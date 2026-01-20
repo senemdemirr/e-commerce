3dd23a8da0357ca5631f531a2617a51d35bcb393
senemdemirr <senemdemir262@gmail.com>
2026-01-20 15:07:26 +0300

    added context for cart

---
+++ context/CartContext.js
@@ -0,0 +1,66 @@
+"use client";
+import { apiFetch } from "@/lib/apiFetch/fetch";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { createContext, useContext } from "react";
+
+const CartContext = createContext(null);
+
+export function CartProvider({ children }) {
+    const [quantity, setQuantity] = useState(0);
+    const [loading, setLoading] = useState(false);
+    const router = useRouter();
+
+    useEffect(() => {
+        const fetchCart = async () => {
+            try {
+                const res = await apiFetch("/api/cart");
+                if (res?.totalQuantity) {
+                    setQuantity(res.totalQuantity);
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchCart();
+    }, []);
+
+    const addToCart = async (product, quantity = 1) => {
+        setLoading(true);
+        try {
+            const res = await apiFetch('/api/cart', {
+                method: "POST",
+                body: JSON.stringify({ productSku: product.sku, quantity })
+            });
+
+            if (res.totalQuantity) {
+                setQuantity(res.totalQuantity);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        finally {
+            setTimeout(() => {
+                setLoading(false);
+                router.push("/basket");
+            }, 2000);
+        }
+    }
+
+
+    return (
+        <CartContext.Provider
+            value={{ addToCart, quantity, loading }}
+        >
+            {children}
+        </CartContext.Provider>
+    );
+
+}
+export function useCart() {
+    const ctx = useContext(CartContext);
+    if (!ctx) {
+        throw new Error("useCart must be used within CartProvider");
+    }
+    return ctx;
+}
