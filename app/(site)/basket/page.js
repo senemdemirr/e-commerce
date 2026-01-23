98fd1f13f4173eea75efffd3b78c206164fe12af
senemdemirr <senemdemir262@gmail.com>
2026-01-23 15:00:38 +0300

    if user has cart items display them

--- app/(site)/basket/page.js
+++ app/(site)/basket/page.js
@@ -1,8 +1,27 @@
-export default function BasketPage(){
+"use client";
+import { useCart } from "@/context/CartContext";
+
+export default function BasketPage() {
+    const { items } = useCart();
+
     return (
         <div className="container mx-auto p-4">
             <h1 className="text-2xl font-bold mb-4">My Basket</h1>
-            <p>This is where your basket items will be displayed.</p>
+
+            {items.length > 0 ? (
+                <div className="flex flex-col gap-8 lg:flex-row lg:gap-12">
+                    <div className="flex flex-col gap-6">
+                        {items.map((item) => (
+                            <li key={item.id} className="border p-2 rounded">
+                                {item.title}
+                            </li>
+                        ))}
+
+                    </div>
+                </div>
+            ) : (
+                <p>This is where your basket items will be displayed.</p>
+            )}
         </div>
     );
 }
