e7edd1b9dbf895dfc568ff05e7d6dc7ea0f2c229
senemdemirr <senemdemir262@gmail.com>
2026-01-23 17:33:53 +0300

    added tailwind conf

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('postcss-load-config').Config} */
 export default {
   plugins: {
     '@tailwindcss/postcss': {},
---
+++ tailwind.config.mjs
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: "class",
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "primary": "#8dc8a1", // Mint Green
+                "primary-dark": "#7ab38e",
+                "secondary": "#A0C8A0", // Soft Green
+                "accent": "#F0B48C", // Pastel Orange
+                "background-light": "#f8f9fa",
+                "background-dark": "#151d18",
+                "surface-light": "#ffffff",
+                "surface-dark": "#1e2621",
+                "text-main": "#131614",
+                "text-muted": "#6d7e73",
+            },
+            fontFamily: {
+                "display": ["Plus Jakarta Sans", "sans-serif"],
+                "body": ["Noto Sans", "sans-serif"]
+            },
+            borderRadius: {
+                "DEFAULT": "0.5rem",
+                "lg": "1rem",
+                "xl": "1.5rem",
+                "2xl": "2rem",
+                "full": "9999px"
+            },
+        },
+    },
+    plugins: [],
+}
