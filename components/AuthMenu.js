4a34dd6e92669ab81c004fb082d2675de3c7b387
senemdemirr <senemdemir262@gmail.com>
2026-02-06 17:01:12 +0300

    It was translated into English and a new tab was added under the dropdown.

--- components/AuthMenu.js
+++ components/AuthMenu.js
@@ -10,30 +10,35 @@ export default function AuthMenu() {
             <ul className="px-4 py-2 flex">
                 <li className="cursor-pointer relative group">
                     <PersonIcon sx={{ color: "#6E7982", width: "21px", height: "21px" }}></PersonIcon>
-                    <span className="ms-2 !text-gray-600 text-[14.5px]">{user ? "HESABIM" :"SIGN IN"}</span>
+                    <span className="ms-2 !text-gray-600 text-[14.5px]">{user ? "MY ACCOUNT" : "SIGN IN"}</span>
 
                     <ul className="invisible absolute left-0 top-full z-50 mt-2 w-48 rounded-md bg-white shadow-lg opacity-0 transition-all duration-150 group-hover:visible group-hover:opacity-100">
                         <li>
                             {user ? (
                                 <Link href={"/my-profile/user-information"} className="block px-4 py-2 text-sm hover:bg-gray-100"> My Profile
                                 </Link>
-                            ) 
-                            : 
-                            (
-                                <a href="/auth/login" className="block px-4 py-2 text-sm hover:bg-gray-100"> Sign in
-                                </a>
-                            )}
+                            )
+                                :
+                                (
+                                    <a href="/auth/login" className="block px-4 py-2 text-sm hover:bg-gray-100"> Sign in
+                                    </a>
+                                )}
                         </li>
-                        {user ? 
-                        (
-                            <a href="/auth/logout" className="block px-4 py-2 text-sm hover:bg-gray-100">
-                                Log Out
-                            </a>
-                        ) 
-                        : 
-                        (
-                            ""
-                        )
+                        {user ?
+                            (
+                                <>
+                                    <a href="/my-profile/orders" className="block px-4 py-2 text-sm hover:bg-gray-100">
+                                        My Orders
+                                    </a>
+                                    <a href="/auth/logout" className="block px-4 py-2 text-sm hover:bg-gray-100">
+                                        Log Out
+                                    </a>
+                                </>
+                            )
+                            :
+                            (
+                                ""
+                            )
                         }
 
                     </ul>
