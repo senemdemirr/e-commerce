7bc5f47e91b9c4376c820c16b5411f604a3e91ba
senemdemirr <senemdemir262@gmail.com>
2026-02-14 16:43:25 +0300

    fix: Update iyzipay dependency to version 2.0.65 and remove outputFileTracingIncludes from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     serverExternalPackages: ['iyzipay'],
-    outputFileTracingIncludes: {
-        '/api/checkout': [
-            'node_modules/iyzipay/lib/resources/**',
-            'node_modules/iyzipay/lib/requests/**'
-        ],
-        '/api/payment': [
-            'node_modules/iyzipay/lib/resources/**',
-            'node_modules/iyzipay/lib/requests/**'
-        ]
-    }
 };
 
 export default nextConfig;
