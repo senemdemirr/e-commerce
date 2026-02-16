271d0774f2e69cb96aaabe8c360d1e6455df540a
senemdemirr <senemdemir262@gmail.com>
2026-02-16 17:46:01 +0300

    refactor: enhance email verification prompt in EmailVerified component

--- components/EmailVerified.js
+++ components/EmailVerified.js
@@ -15,7 +15,7 @@ export default function EmailVerified() {
   }
   return (
     <div className="container w-full mx-auto flex flex-row border border-gray-300 p-3 rounded mt-4 justify-between align-center">
-      <Typography className="text-orange-500">Did you verified your email?</Typography>
+      <Typography className="text-orange-500">Did you verified your email? Please check your inbox and verify your email.</Typography>
       <div className="flex flex-row align-center">
         <Button href={"/auth/login?prompt=login&returnTo=/my-profile/user-information"} variant="contained" className="button cursor-pointer !no-underline text-center !me-2">YES</Button>
         <Button onClick={showVerifyEmailMessage} variant="contained">No</Button>
